10ce838d33b6e0a09c36b07a0c0048580d09ae64
Logan Halverson <logan.halverson@clareitysecurity.com>
2017-04-11 12:07:31 -0700

    Stages working and infinite scroll and spawn of stuff

--- javascript/gameTitle.js
+++ javascript/gameTitle.js
@@ -4,18 +4,19 @@ var InfiniteScroller = InfiniteScroller || {};
 //loading the game assets
 InfiniteScroller.GameTitle = function () { };
 
-InfiniteScroller.Preload.prototype = {
+InfiniteScroller.GameTitle.prototype = {
     preload: function () {
-       
+        this.game.load.image('play', 'assets/play-text.png');
     },
     create: function () {
+        this.game.stage.backgroundColor = '#999999';
         titleText = this.game.add.text(16, 16, 'Helo Fly', { fontSize: '32px' });
         titleText.fixedToCamera = true;
         titleText.stroke = '#000000';
         titleText.strokeThickness = 8;
         titleText.fill = '#43d637';
 
-        var playButton = this.game.add.button(160, 320, "play", this.playTheGame, this);
+        var playButton = this.game.add.button(this.game.width/2, this.game.height/2, 'play', this.playTheGame, this);
         playButton.anchor.setTo(0.5, 0.5);
     },
     playTheGame: function () {
